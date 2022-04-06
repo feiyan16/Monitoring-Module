f69d5dfad607eba8ef1bbaeea9c00bcc9edea9ad
Isaac-Duke-Blackwood <isaac.duke.blackwood@gmail.com>
2022-04-06 00:59:54 -0500

    added event handlers for stop and disconnect
    I added the event handlers, and a dict keyed by a tuple of sid and port which regulates when the daemons should stop. I can't tell how much this is working because I haven't dove deeply into the front-end javascript, but some of the threads at least close so this seems like an improvement.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from threading import Thread
 from flask import Flask
 import socketio
@@ -16,6 +17,7 @@ def main():
     app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
     # TODO: add way to track whether threads for specific ports should be alive?
+    daemon_dict = {}
 
     # Define function to be called when there is a start message
     @sio.on('start')
@@ -23,12 +25,23 @@ def main():
         if DEBUG:
             print("Received start:\n" +
                   "sid: " + str(sid) + "\n" +
-                  "data: " + str(port))
+                  "port: " + str(port))
         # TODO: may want to add error checking to this to ensure the msg is a properly formatted port number
         port_number = str(port)
 
+        if (str(sid), str(port)) in daemon_dict:
+            if daemon_dict[(str(sid), str(port))]:
+                # A thread for this already exists so skip creating it
+                if DEBUG:
+                    print("WARNING: daemon already exists with tuple: (" + str(sid) + ", " + str(port) + ")")
+                return
+
+        daemon_dict[(str(sid), str(port))] = True
+        if DEBUG:
+            print("daemon_dict: " + str(daemon_dict))
+
         # Call the monitor function and send to dashboard in a loop
-        while True:
+        while daemon_dict[(str(sid), str(port))]:
             metrics = process_packets(port=port_number)
             # Convert metrics and stream identifier to JSON and send to dashboard
             new_msg = {
@@ -39,18 +52,31 @@ def main():
                 print("Sending to " + str(ADDRESS) + " on port " + str(PORT) + ":\n" + str(new_msg) + "\n")
             sio.emit(port_number, new_msg, room=sid)
 
+        if DEBUG:
+            print("daemon ending with tuple: (" + str(sid) + ", " + str(port) + ")")
+
+    @sio.on('stop')
+    def start_monitor_thread(sid, port):
+        if DEBUG:
+            print("Received stop:\n" +
+                  "sid: " + str(sid) + "\n" +
+                  "port: " + str(port))
+        daemon_dict[(str(sid), str(port))] = False
+        if DEBUG:
+            print("daemon_dict: " + str(daemon_dict))
+
+    @sio.event
+    def disconnect(sid):
+        print("Received stop:\n" +
+              "sid: " + str(sid))
+        for (sid, port) in daemon_dict:
+            daemon_dict[(sid, port)] = False
+
     if DEBUG:
         print("Listening to " + str(ADDRESS) + " on port " + str(PORT) + "\n")
 
     if __name__ == '__main__':
         app.run(port=PORT)
-
-    # @sio.on('stop')
-    # def start_monitor_thread(sid, port):
-    #    if DEBUG:
-    #        print("Received start:\n" +
-    #            "sid:" + str(sid) + "\n" +
-    #            "data:" + str(port))
 
 
 # driving function to process data throughput of a specified port/interface.
