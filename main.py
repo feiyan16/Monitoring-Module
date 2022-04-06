48979e2ab67872572dc9c024788e97eade7659ee
Isaac-Duke-Blackwood <isaac.duke.blackwood@gmail.com>
2022-04-05 23:57:09 -0500

    switched from eventlet to flask
    currently, there aren't random exceptions being thrown. So, that is an improvement. However, these changes seem to still have the major issue of threads never closing which obviously, would be lovely to fix.

--- main.py
+++ main.py
@@ -1,51 +1,56 @@
 from threading import Thread
+from flask import Flask
 import socketio
-import json
 import pyshark
-import eventlet
 
 ADDRESS = "localhost"
 PORT = 5555
 DEBUG = True
 
-eventlet.monkey_patch()
 
 def main():
     # Create a listener for new connections on the specified socket
-    sio = socketio.Server(logger=True, cors_allowed_origins='http://localhost:3000')
+    sio = socketio.Server(logger=DEBUG, engineio_logger=False, async_mode='threading',
+                          cors_allowed_origins='http://localhost:3000')
+    app = Flask(__name__)
+    app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
+
+    # TODO: add way to track whether threads for specific ports should be alive?
 
     # Define function to be called when there is a start message
     @sio.on('start')
-    def start_monitor_thread(sid, data):
+    def start_monitor_thread(sid, port):
         if DEBUG:
-            print('sid:', sid)
-            print('data:' + str(data))
-        stream_thread = Thread(target=monitor_stream, args=(sio, data))
-        stream_thread.daemon = True
-        stream_thread.start()
+            print("Received start:\n" +
+                  "sid: " + str(sid) + "\n" +
+                  "data: " + str(port))
+        # TODO: may want to add error checking to this to ensure the msg is a properly formatted port number
+        port_number = str(port)
+
+        # Call the monitor function and send to dashboard in a loop
+        while True:
+            metrics = process_packets(port=port_number)
+            # Convert metrics and stream identifier to JSON and send to dashboard
+            new_msg = {
+                "metrics": metrics,
+                "stream_id": port_number
+            }
+            if DEBUG:
+                print("Sending to " + str(ADDRESS) + " on port " + str(PORT) + ":\n" + str(new_msg) + "\n")
+            sio.emit(port_number, new_msg, room=sid)
 
     if DEBUG:
         print("Listening to " + str(ADDRESS) + " on port " + str(PORT) + "\n")
 
-    app = socketio.WSGIApp(sio)
-    eventlet.wsgi.server(eventlet.listen(('localhost', PORT)), app)
+    if __name__ == '__main__':
+        app.run(port=PORT)
 
-
-def monitor_stream(sio, port):
-    # TODO: may want to add error checking to this to ensure the msg is a properly formatted port number
-    port_number = str(port)
-
-    # Call the monitor function and send to dashboard in a loop
-    while True:
-        metrics = process_packets(port=port_number)
-        # Convert metrics and stream identifier to JSON and send to dashboard
-        new_msg = {
-            "metrics": metrics,
-            "stream_id": port_number
-        }
-        if DEBUG:
-            print("Sending to " + str(ADDRESS) + " on port " + str(PORT) + ":\n" + str(new_msg) + "\n")
-        sio.emit(port_number, new_msg)
+    # @sio.on('stop')
+    # def start_monitor_thread(sid, port):
+    #    if DEBUG:
+    #        print("Received start:\n" +
+    #            "sid:" + str(sid) + "\n" +
+    #            "data:" + str(port))
 
 
 # driving function to process data throughput of a specified port/interface.
