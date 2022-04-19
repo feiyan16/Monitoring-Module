7896748b91287abc9c9f5498fbc87ac79da503a5
Isaac-Duke-Blackwood <isaac.duke.blackwood@gmail.com>
2022-04-19 14:40:21 -0500

    Create app.py
    Created file that can be run via command:
    waitress-serve --threads=50 --listen=127.0.0.1:5555 app:app

---
+++ app.py
@@ -0,0 +1,91 @@
+import socketio
+import pyshark
+
+ADDRESS = "localhost"
+PORT = 5555
+DEBUG = True
+
+# Create a listener for new connections on the specified socket
+sio = socketio.Server(logger=DEBUG, engineio_logger=False,
+                      cors_allowed_origins='http://localhost:3000')
+app = socketio.WSGIApp(sio)
+
+# TODO: add way to track whether threads for specific ports should be alive?
+daemon_dict = {}
+
+
+# Define function to be called when there is a start message
+@sio.on('start')
+def start_monitor_thread(sid, port):
+    if DEBUG:
+        print("Received start:\n" +
+              "sid: " + str(sid) + "\n" +
+              "port: " + str(port))
+    # TODO: may want to add error checking to this to ensure the msg is a properly formatted port number
+    port_number = str(port)
+
+    if (str(sid), str(port)) in daemon_dict:
+        if daemon_dict[(str(sid), str(port))]:
+            # A thread for this already exists so skip creating it
+            if DEBUG:
+                print("WARNING: daemon already exists with tuple: (" + str(sid) + ", " + str(port) + ")")
+            return
+
+    daemon_dict[(str(sid), str(port))] = True
+    if DEBUG:
+        print("daemon_dict: " + str(daemon_dict))
+
+    # Call the monitor function and send to dashboard in a loop
+    while daemon_dict[(str(sid), str(port))]:
+        metrics = process_packets(port=port_number)
+        # Convert metrics and stream identifier to JSON and send to dashboard
+        new_msg = {
+            "metrics": metrics,
+            "stream_id": port_number
+        }
+        if DEBUG:
+            print("Sending to " + str(ADDRESS) + " on port " + str(PORT) + ":\n" + str(new_msg) + "\n")
+        sio.emit(port_number, new_msg, room=sid)
+
+    if DEBUG:
+        print("daemon ending with tuple: (" + str(sid) + ", " + str(port) + ")")
+
+
+@sio.on('stop')
+def start_monitor_thread(sid, port):
+    if DEBUG:
+        print("Received stop:\n" +
+              "sid: " + str(sid) + "\n" +
+              "port: " + str(port))
+    daemon_dict[(str(sid), str(port))] = False
+    if DEBUG:
+        print("daemon_dict: " + str(daemon_dict))
+
+
+@sio.event
+def disconnect(sid):
+    print("Received stop:\n" +
+          "sid: " + str(sid))
+    for (sid, port) in daemon_dict:
+        daemon_dict[(sid, port)] = False
+
+
+if DEBUG:
+    print("Listening to " + str(ADDRESS) + " on port " + str(PORT) + "\n")
+
+
+# driving function to process data throughput of a specified port/interface.
+# Port: Port used to filter packets received from the interface
+def process_packets(port):
+    packet_filter = 'port ' + port
+    capture = pyshark.LiveCapture(interface='lo', bpf_filter=packet_filter)
+
+    capture.sniff(timeout=2)
+    data_received = 0
+    print(capture)
+    if len(capture) == 0:
+        return 0
+
+    for i in range(0, len(capture)):
+        data_received = data_received + int(capture[i].length)
+    return data_received
